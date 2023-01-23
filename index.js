b42808f4516426d81360f278970b27b4db0ade7a
mininekos <manuel2g2m@gmail.com>
2023-01-23 09:35:54 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express= require('express')
+
+const app=express()
+app.use(express.json())
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'Alumnos'
+let db =null
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!')
+    }
+    db = client.db(databaseName)
+
+})
+
+app.post('/crearcontacto',(req,res)=>{
+    db.collection('Alumno').insertOne({
+        dni:'123',
+        nombre:'',
+        edad: 12,
+        telefono:'123456789'
+    })
+    res.send({
+        dni:'123',
+        nombre:'',
+        edad: 12,
+        telefono:'123456789'})
+})
+
+app.get('/buscarcontacto',(req,res)=>{
+
+})
+
+app.put('/actualizacontacto',(req,res)=>{
+
+})
+
+app.delete('/borracontacto',(req,res)=>{
+
+})
+
+app.listen(3000,)
