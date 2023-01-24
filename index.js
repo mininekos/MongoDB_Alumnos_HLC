1eef6a5ea29fd04e2788645f776e6267cfe74f8b
mininekos <manuel2g2m@gmail.com>
2023-01-24 14:11:36 +0100

    v2

--- index.js
+++ index.js
@@ -15,33 +15,59 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
         return console.log('Unable to connect to database!')
     }
     db = client.db(databaseName)
-
+   
 })
 
 app.post('/crearcontacto',(req,res)=>{
-    db.collection('Alumno').insertOne({
-        dni:'123',
-        nombre:'',
-        edad: 12,
-        telefono:'123456789'
-    })
-    res.send({
-        dni:'123',
-        nombre:'',
-        edad: 12,
-        telefono:'123456789'})
-    })
-
-app.get('/buscarcontacto',(req,res)=>{
-
+    db.collection('Alumno').insertOne(req.body).
+    then(data => 
+        res.send({
+            id:data.insertedId})
+        )
+        .catch(err => 
+            res.send(err)
+        ) 
 })
 
-app.put('/actualizacontacto',(req,res)=>{
+app.get('/buscarcontacto',(req,res)=>{
+    db.collection('Alumno').findOne({_id:new mongodb.ObjectId(req.body.id)})
+    .then(data =>
+        res.send({
+            nombre:data.nombre
+        })
+    )
+    .catch(err=>
+        res.send(err))
+})
 
+app.put('/actualizarcontacto',(req,res)=>{
+    db.collection('Alumno').findOne({_id:new mongodb.ObjectId(req.body.id)})
+    .then(data =>{
+        if(data!=null){
+            db.collection('Alumno').updateOne(data,{
+                $set:{
+                    dni:req.body.dni,
+                    nombre:req.body.nombre,
+                    edad:req.body.edad,
+                    telefono:req.body.telefono
+                }
+            });
+            res.send({estado:"Todo correcto"})
+            res.send(req.body)
+        }
+        else{
+            res.send(
+                {estado:"Error"}
+            )
+        }
+        }
+    )
+    .catch(err=>
+        res.send(err))
 })
 
 app.delete('/borracontacto',(req,res)=>{
 
 })
 
-app.listen(3000,)
+app.listen(3000)
